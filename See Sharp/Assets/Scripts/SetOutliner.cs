6f7ccc8b6d49a01507868332a7e0ecf466c6a6a6
Celis <celistek92@gmail.com>
2020-09-27 14:28:54 -0500

    WIP - set outline for objects

---
+++ See Sharp/Assets/Scripts/SetOutliner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetOutliner : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+    var outline = gameObject.AddComponent<Outline>();
+    outline.OutlineMode = Outline.Mode.OutlineAll;
+    outline.OutlineColor = Color.yellow;
+    outline.OutlineWidth = 5f;
+    }
+}
