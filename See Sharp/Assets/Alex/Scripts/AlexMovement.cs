e0f79613b0a81e26dc195d77c7d6d9d1f76d4ab9
Manuel G <A01701414@itesm.mx>
2020-10-09 23:43:50 -0500

    Alex Movement, rotation gravity and collisions

---
+++ See Sharp/Assets/Alex/Scripts/AlexMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlexMovement : MonoBehaviour
+{
+    public float turnSpeed = 20f;
+
+    Animator m_Animator;
+    Rigidbody m_Rigidbody;
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Debug.Log("Horizontral   " + horizontal);
+        Debug.Log("Vertical   " + (vertical));
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        Debug.Log(m_Movement);
+        m_Animator.SetBool("IsWalking", isWalking);
+        Debug.Log("Esto es el is walking  " + m_Animator.GetBool("IsWalking"));
+        Debug.Log(m_Animator.deltaPosition.magnitude);
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    void OnAnimatorMove()
+    {
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * 0.03f);
+        m_Rigidbody.MoveRotation(m_Rotation);
+    }
+}
