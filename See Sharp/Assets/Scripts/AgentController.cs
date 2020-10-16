53f8fc7600500ea6df59743bbbd136cddc63e1b8
FranciscoPalaciosA <francisco@nivelate.mx>
2020-10-16 16:02:39 -0600

    Player moves

---
+++ See Sharp/Assets/Scripts/AgentController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentController : MonoBehaviour
+{
+    IInput input;
+    AgentMovement movement;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        input = GetComponent<IInput>();
+        movement = GetComponent<AgentMovement>();
+        input.OnMovementDirectionInput += movement.HandleMovementDirection;
+        input.OnMovementInput += movement.HandleMovement;
+    }
+
+    private void OnDisable()
+    {
+        input.OnMovementDirectionInput -= movement.HandleMovementDirection;
+        input.OnMovementInput -= movement.HandleMovement;
+    }
+
+}
