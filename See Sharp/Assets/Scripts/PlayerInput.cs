8d6517675d56a15a7158092a40002eebd092f71d
Manuel G <A01701414@itesm.mx>
2020-10-16 15:33:31 -0500

    Video 1 and 2

---
+++ See Sharp/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour, IInput
+{
+    public Action<Vector2> OnMovementInput { get; set; }
+    public Action<Vector3> OnMovementDirectionInput { get; set; }
+
+    private void Start()
+    {
+        //To move in the direction of the cinemachine
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    private void Update()
+    {
+        GetMovementInput();
+        GetMovementDirection();
+    }
+
+    private void GetMovementDirection()
+    {
+        var cameraForewardDirection = Camera.main.transform.forward;
+        Debug.DrawRay(Camera.main.transform.position, cameraForewardDirection * 10, Color.red);
+        var directionToMoveIn = Vector3.Scale(cameraForewardDirection, (Vector3.right + Vector3.forward));
+        Debug.DrawRay(Camera.main.transform.position, directionToMoveIn * 10, Color.blue);
+        OnMovementDirectionInput?.Invoke(directionToMoveIn);
+    }
+
+    private void GetMovementInput()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        OnMovementInput?.Invoke(input);
+    }
+}
