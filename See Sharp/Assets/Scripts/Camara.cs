f06970a826072f6c0534ff0165d5244d84d1c002
Celis <celistek92@gmail.com>
2020-10-20 18:26:09 -0500

    Added wireframe shader

---
+++ See Sharp/Assets/Scripts/Camara.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnPreRender() {
+        GL.wireframe = true;
+    }
+
+    private void OnPostRender() {
+        GL.wireframe = false;
+    }
+}
--- See Sharp/Assets/Scripts/SetOutliner.cs
+++ See Sharp/Assets/Scripts/SetOutliner.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class SetOutliner : MonoBehaviour
 {
+    [SerializeField]
+    private Material firstMaterial;
 
+    [SerializeField]
+    private Material secondMaterial;
     // Update is called once per frame
     void Update()
     {
-    
+        Debug.Log('1');
+     if(Input.GetKey(KeyCode.A)){
+        Debug.Log("Pressed");
+        gameObject.GetComponent<Renderer>().sharedMaterial.SetFloat("_Thickness", 2);
+        gameObject.GetComponent<Renderer>().material = secondMaterial;
+     }
+    }
+    private void Start() {
+        secondMaterial = Resources.Load<Material>("Materials/whiteOutliner");
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        firstMaterial = meshRenderer.material;
     }
 
     private void setOutline (string color, int mode, bool precomputed, float width){
