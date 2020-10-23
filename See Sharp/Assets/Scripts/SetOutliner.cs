5e17f00eaf98556793903cef0459d0795294b6b8
Celis <celistek92@gmail.com>
2020-10-23 14:31:49 -0500

    new Script

--- See Sharp/Assets/Scripts/SetOutliner.cs
+++ See Sharp/Assets/Scripts/SetOutliner.cs
@@ -4,25 +4,30 @@ using UnityEngine;
 
 public class SetOutliner : MonoBehaviour
 {
-    [SerializeField]
-    private Material firstMaterial;
+    private Renderer render;
 
-    [SerializeField]
-    private Material secondMaterial;
+    private float thick;
+    private bool increase;
     // Update is called once per frame
     void Update()
     {
         Debug.Log('1');
-     if(Input.GetKey(KeyCode.A)){
         Debug.Log("Pressed");
-        gameObject.GetComponent<Renderer>().sharedMaterial.SetFloat("_Thickness", 2);
-        gameObject.GetComponent<Renderer>().material = secondMaterial;
+        if(thick > 3){
+            thick = 3f;
+        }
+        render.material.SetFloat("_Thickness", thick);
+        render.material.SetColor("_Color",new Color(1f,1f,1f,1f));
+        // gameObject.GetComponent<Renderer>().material = secondMaterial;
+        thick+=0.2f;
+     
+     if(Input.GetKey(KeyCode.S)){
+            render.material.SetFloat("_Thickness", 0.01f);
      }
     }
     private void Start() {
-        secondMaterial = Resources.Load<Material>("Materials/whiteOutliner");
-        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
-        firstMaterial = meshRenderer.material;
+        render = gameObject.GetComponent<Renderer>();
+        thick = 0.05f;
     }
 
     private void setOutline (string color, int mode, bool precomputed, float width){
