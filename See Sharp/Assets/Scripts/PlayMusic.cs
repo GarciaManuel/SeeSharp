63c7367bd691f438a1e7794c1aac0491da2904a9
Celis <celistek92@gmail.com>
2020-11-26 22:14:14 -0600

    Music added to menu and levels

---
+++ See Sharp/Assets/Scripts/PlayMusic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMusic : MonoBehaviour
+{
+    public AudioSource music;
+    void Start()
+    {
+        music.enabled = true;
+        music.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
